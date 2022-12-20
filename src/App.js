50ad80fc4c8a420305f9bed52012aa8c4dc2d3d9
AhmadiGu <ahmadigulalai76@gmail.com>
2022-12-20 21:55:10 +0500

    Display the pages in one page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
+import { Route, Routes } from 'react-router-dom';
 import Calculator from './components/Calculator';
+import Navbar from './components/Navigation';
+import Home from './components/pages/Home';
+import Quote from './components/pages/Quote';
 
 class App extends Component {
   constructor(props) {
@@ -10,7 +14,22 @@ class App extends Component {
   render() {
     return (
       <div className="main-app">
-        <Calculator />
+
+        <Navbar />
+        <Routes>
+          <Route
+            path="/math-magicians/"
+            element={(<Home />)}
+          />
+          <Route
+            path="/math-magicians/calculator"
+            element={(<Calculator />)}
+          />
+          <Route
+            path="/math-magicians/quote"
+            element={(<Quote />)}
+          />
+        </Routes>
       </div>
     );
   }
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -13,44 +13,47 @@ const Calculator = () => {
   const { operation } = state;
 
   return (
-    <div className="container">
-      <div className="calculate-place">
-        {total}
-        {operation}
-        {next}
+    <div className="calculater">
+      <div className="side-p"><p>Let&apos;s Do Some Math!</p></div>
+      <div className="container">
+        <div className="calculate-place">
+          {total}
+          {operation}
+          {next}
 
-      </div>
-      <div className="functions-place">
+        </div>
+        <div className="functions-place">
 
-        <div className="rows">
-          <button type="button" onClick={buttonClick} className="btn-gray">AC</button>
-          <button type="button" onClick={buttonClick} className="btn-gray">+/-</button>
-          <button type="button" onClick={buttonClick} className="btn-gray">%</button>
-          <button type="button" onClick={buttonClick} className="btn-orang">÷</button>
-        </div>
+          <div className="rows">
+            <button type="button" onClick={buttonClick} className="btn-gray">AC</button>
+            <button type="button" onClick={buttonClick} className="btn-gray">+/-</button>
+            <button type="button" onClick={buttonClick} className="btn-gray">%</button>
+            <button type="button" onClick={buttonClick} className="btn-orang">÷</button>
+          </div>
 
-        <div className="rows">
-          <button type="button" onClick={buttonClick} className="btn-gray">7</button>
-          <button type="button" onClick={buttonClick} className="btn-gray">8</button>
-          <button type="button" onClick={buttonClick} className="btn-gray">9</button>
-          <button type="button" onClick={buttonClick} className="btn-orang">x</button>
-        </div>
-        <div className="rows">
-          <button type="button" onClick={buttonClick} className="btn-gray">4</button>
-          <button type="button" onClick={buttonClick} className="btn-gray">5</button>
-          <button type="button" onClick={buttonClick} className="btn-gray">6</button>
-          <button type="button" onClick={buttonClick} className="btn-orang">-</button>
-        </div>
-        <div className="rows">
-          <button type="button" onClick={buttonClick} className="btn-gray">1</button>
-          <button type="button" onClick={buttonClick} className="btn-gray">2</button>
-          <button type="button" onClick={buttonClick} className="btn-gray">3</button>
-          <button type="button" onClick={buttonClick} className="btn-orang">+</button>
-        </div>
-        <div className="rows">
-          <button type="button" onClick={buttonClick} className="btn-gray zero">0</button>
-          <button type="button" onClick={buttonClick} className="btn-gray">.</button>
-          <button type="button" onClick={buttonClick} className="btn-orang">=</button>
+          <div className="rows">
+            <button type="button" onClick={buttonClick} className="btn-gray">7</button>
+            <button type="button" onClick={buttonClick} className="btn-gray">8</button>
+            <button type="button" onClick={buttonClick} className="btn-gray">9</button>
+            <button type="button" onClick={buttonClick} className="btn-orang">x</button>
+          </div>
+          <div className="rows">
+            <button type="button" onClick={buttonClick} className="btn-gray">4</button>
+            <button type="button" onClick={buttonClick} className="btn-gray">5</button>
+            <button type="button" onClick={buttonClick} className="btn-gray">6</button>
+            <button type="button" onClick={buttonClick} className="btn-orang">-</button>
+          </div>
+          <div className="rows">
+            <button type="button" onClick={buttonClick} className="btn-gray">1</button>
+            <button type="button" onClick={buttonClick} className="btn-gray">2</button>
+            <button type="button" onClick={buttonClick} className="btn-gray">3</button>
+            <button type="button" onClick={buttonClick} className="btn-orang">+</button>
+          </div>
+          <div className="rows">
+            <button type="button" onClick={buttonClick} className="btn-gray zero">0</button>
+            <button type="button" onClick={buttonClick} className="btn-gray">.</button>
+            <button type="button" onClick={buttonClick} className="btn-orang">=</button>
+          </div>
         </div>
       </div>
     </div>
